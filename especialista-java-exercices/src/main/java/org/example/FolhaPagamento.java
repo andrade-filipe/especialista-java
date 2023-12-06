7d69f1c77faa0e5971dc1e8f0988eac540e38b42
andrade-filipe <filipeandrade.work@gmail.com>
2023-12-06 15:44:52 -0300

    desafio composição de objetos e métodos

--- especialista-java-exercices/src/main/java/org/example/FolhaPagamento.java
+++ especialista-java-exercices/src/main/java/org/example/FolhaPagamento.java
@@ -4,14 +4,22 @@ public class FolhaPagamento {
     public FolhaPagamento() {
     }
 
-    double calcularSalario(int qntHoraNormal,
+    Holerite calcularSalario(int qntHoraNormal,
                            int qntHoraExtra,
                            ContratoTrabalho contrato) {
         double pagamentoHoraExtra = qntHoraExtra * contrato.getValorHoraExtra();
         double pagamentoHoraNormal = qntHoraNormal * contrato.getValorHoraNormal();
+
+        Holerite holerite = new Holerite();
+        holerite.setFuncionario(contrato.getFuncionario());
+        holerite.setValorTotalNormal(pagamentoHoraNormal);
+        holerite.setValorTotalExtra(pagamentoHoraExtra);
+        holerite.setValorAdicionalFilhos(0);
+
         if(contrato.hasChildrenAditionalFee()){
-            return (pagamentoHoraNormal + pagamentoHoraExtra) * 1.10;
+            holerite.setValorAdicionalFilhos((pagamentoHoraNormal + pagamentoHoraExtra) * 0.10);
+            return holerite;
         }
-        return pagamentoHoraNormal + pagamentoHoraExtra;
+        return holerite;
     }
 }
---
+++ especialista-java-exercices/src/main/java/org/example/Holerite.java
@@ -0,0 +1,57 @@
+package org.example;
+
+public class Holerite {
+    private Funcionario funcionario;
+    private double valorTotalNormal;
+    private double valorTotalExtra;
+    private double valorAdicionalFilhos;
+
+    public Holerite() {
+    }
+
+    public Funcionario getFuncionario() {
+        return funcionario;
+    }
+
+    public void setFuncionario(Funcionario funcionario) {
+        this.funcionario = funcionario;
+    }
+
+    public double getValorTotalNormal() {
+        return valorTotalNormal;
+    }
+
+    public void setValorTotalNormal(double valorTotalNormal) {
+        this.valorTotalNormal = valorTotalNormal;
+    }
+
+    public double getValorTotalExtra() {
+        return valorTotalExtra;
+    }
+
+    public void setValorTotalExtra(double valorTotalExtra) {
+        this.valorTotalExtra = valorTotalExtra;
+    }
+
+    public double getValorAdicionalFilhos() {
+        return valorAdicionalFilhos;
+    }
+
+    public void setValorAdicionalFilhos(double valorAdicionalFilhos) {
+        this.valorAdicionalFilhos = valorAdicionalFilhos;
+    }
+
+    public double calcularTotal(){
+        return valorTotalExtra + valorTotalNormal + valorAdicionalFilhos;
+    }
+
+    @Override
+    public String toString() {
+        return "Holerite{" +
+            "funcionario=" + funcionario +
+            ", valorTotalNormal=" + valorTotalNormal +
+            ", valorTotalExtra=" + valorTotalExtra +
+            ", valorAdicionalFilhos=" + valorAdicionalFilhos +
+            '}';
+    }
+}
